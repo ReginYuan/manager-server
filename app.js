b56e0854a25da9d69150501a813c937c3ba95237
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2021-06-13 10:54:08 +0800

    6-2 通过jwt生成token

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const log4j = require('./utils/log4j')
-
+const util = require('./utils/util')
+// 生成token
+const jwt = require('jsonwebtoken')
+// 验证token的密钥
+const koajwt = require('koa-jwt')
 
 const users = require('./routes/users')
 const router = require('koa-router')()
@@ -36,16 +40,39 @@ app.use(views(__dirname + '/views', {
 // logger
 app.use(async (ctx, next) => {
   const start = new Date()
-  await next()
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
   console.log(`get params:${JSON.stringify(ctx.request.query)} `)
   console.log(`post params:${JSON.stringify(ctx.request.body)} `)
   log4j.info(`log output info`)
+  await next()
+  // await next().catch((err) => {
+  //   if (err.status == '401') {
+  //     ctx.status == 200;
+  //     ctx.body = util.fail('Token认证失败', util.CODE.AUTH_ERROR)
+  //   } else {
+  //     throw err;
+  //   }
+  // })
 })
 
+
+// 统一验证token的密钥
+// app.use(koajwt({ secret: 'regin' }))
 // routes 代理  一级路由
 router.prefix("/api")
+router.get('/leave/count', (ctx) => {
+  // 从前端获取token
+  const token = ctx.request.headers.authorization.split('')[1]
+  // 解密token前端密钥
+  const data = jwt.verify(token, { entries: 'regin' })
+  ctx.body = 'hello count' + data;
+})
+
+router.get('/menu/list', (ctx) => {
+  ctx.body = 'hello list';
+})
+
 // 二级路由
 router.use(users.routes(), users.allowedMethods())
 // 加载路由
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,12 @@
 const router = require('koa-router')()
 const User = require('./../models/userSchema.js')
 const util = require("./../utils/util.js")
+/**
+ * JSON Web 令牌的一种实现。
+ * 这是针对 draft-ietf-oauth-json-web-token-08开发的，它使用了 node-jws。
+ * 
+ * */
+const jwt = require('jsonwebtoken')
 router.prefix('/users')
 
 /**
@@ -18,8 +24,21 @@ router.post('/login', async (ctx) => {
       userName,
       userPwd
     })
+    // data用来存储token
+    const data = res._doc
+
+    // 生成token
+    const token = jwt.sign({
+      data: data,
+    }, 'regin', { expiresIn: 30 })
+
+    console.log('token=>', token)
+
+    // 如果用户的数据存在
     if (res) {
-      ctx.body = util.success(res)
+      // 将token和res数据放进响应头中并放入返回上下文的body中
+      data.token = token;
+      ctx.body = util.success(data);
     } else {
       ctx.body = util.fail("账号或密码不正确")
     }
